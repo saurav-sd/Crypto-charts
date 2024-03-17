395c66508a9e0d38e908b8f2c6df34ba8f90a696
saurav-sd <sauravdaware777@gmail.com>
2024-03-17 12:33:40 +0530

    new changes

--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactECharts from 'echarts-for-react';
 
 const LineChart = ({ data, selectedColumn }) => {
+
+  // console.log("selectedColumn : ", selectedColumn)
+  // console.log("LineChart Data : ", data)
+
+  // var timestamps = data.map(item => parseFloat(item["Unix Timestamp"]));
+  const timestamps = data.map(item => new Date(parseFloat(item["Unix Timestamp"])));
+  console.log("Timestamps : ", timestamps);
+
   const chartData = data.map((item) => ({
-    date: new Date(item.time),
+    date: new Date(item.Date),
     value: parseFloat(item[selectedColumn.slice(1)])
   }));
 
+  console.log("Chart data : ", chartData);
+
   const option = {
     xAxis: {
-      type: 'time'
+      type: 'time', // ??
+      // data: timestamps.map(ts => new Date(ts).toLocaleString()),
+      // axisLabel: {
+      //   formatter: function (value, index) {
+      //     // Customize the formatting of the x-axis labels here
+      //     return new Date(value).toLocaleString(); // Example: Format timestamps to locale string
+      //   }
+      // }
     },
     yAxis: {
       type: 'value'
@@ -17,6 +34,7 @@ const LineChart = ({ data, selectedColumn }) => {
     series: [
       {
         type: 'line',
+        smooth: true,
         data: chartData.map(({ date, value }) => [date, value])
       }
     ]
